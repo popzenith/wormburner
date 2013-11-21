8406ed8e307842253beaf101b8d4a00202ecd55a
Jerome Novosad <hello@popzenith.com>
2013-11-20 20:01:49 -0500

    1.000

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-puts 'SETTING UP MASTER USER LOGIN'
+puts 'SETTING UP USERS'
 user = User.create! :name => 'J.L. Novosad', :email => 'test1@golfpals.co', :password => 'foobar', :password_confirmation => 'foobar'
 puts 'New user created: ' << user.name
 user.add_role :admin
 
-puts 'SETTING UP MASTER USER LOGIN'
-user = User.create! :name => 'Mr. Popular', :email => 'golfpals@popzenith.com', :password => 'foobar', :password_confirmation => 'foobar'
-puts 'New user created: ' << user.name
-user.add_role :admin
+user = User.create! :name => "Al Theman", :email => "test2@golfpals.co", :password => "foobar", :password_confirmation => "foobar"
+user.microposts.create!(content: "Killing it")
+user.microposts.create!(content: "The price is reasonable")
+user.follow!(User.find_by_name("J.L. Novosad"))
+
+user = User.create! :name => "Jake Trout", :email => "test3@golfpals.co", :password => "foobar", :password_confirmation => "foobar"
+user.microposts.create!(content: "Are we doing this?")
+user.microposts.create!(content: "Say hi to Jimmy, the manager")
+user.follow!(User.find_by_name("J.L. Novosad"))
+
+user = User.create! :name => "Sandeee", :email => "test4@golfpals.co", :password => "foobar", :password_confirmation => "foobar"
+user.microposts.create!(content: "most of the fairways and greens were well taken care of and the design of the course had a good combination of interesting and challenging holes as well as some more straight forward ones.")
+user.microposts.create!(content: "woooooooooooo")
+user.follow!(User.find_by_name("J.L. Novosad"))
+
+user = User.create! :name => "Kim O'Connor", :email => "test5@golfpals.co", :password => "foobar", :password_confirmation => "foobar"
+user.microposts.create!(content: "Gooooooose")
+user.microposts.create!(content: "Check out the porch bar")
+user.follow!(User.find_by_name("J.L. Novosad"))
+
+user = User.create! :name => "Mr. Popular", :email => "golfpals@popzenith.com", :password => "foobar", :password_confirmation => "foobar"
+user.follow!(User.find_by_name("Al Theman"))
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,55 +1,10 @@
-
 namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
     make_users
-    make_relationships
-    make_popular
-    make_microposts
   end
 end
 
 def make_users
-  10.times do |n|
-    name  = Faker::Name.name
-    email = "example-#{n+1}@golfpals.co"
-    password  = "password"
-    User.create!(name:     name,
-                 email:    email,
-                 password: password,
-                 password_confirmation: password)
-  end
-  89.times do |n|
-    name  = Faker::Name.name
-    email = "example-#{n+11}@golfpals.co"
-    password  = "password"
-    User.create!(name:     name,
-                 email:    email,
-                 password: password,
-                 password_confirmation: password)
-  end
-end
-
-def make_relationships
-  users = User.all
-  user  = users.first
-  followed_users = users[2..50]
-  followers      = users[2..40]
-  followed_users.each { |followed| user.follow!(followed) }
-  followers.each      { |follower| follower.follow!(user) }
-end
-
-def make_popular
-  users = User.all
-  user  = users[2]
-  followed_users = users[3..4]
-  followed_users.each { |followed| user.follow!(followed) }
-end
-
-def make_microposts
-  users = User.all(limit: 50)
-  50.times do
-    content = Faker::Lorem.sentence(5)
-    users.each { |user| user.microposts.create!(content: content) }
-  end
+  
 end
