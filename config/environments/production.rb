bd81e3f6c816b72408901ea5530e2d9e9b2f2f5f
Jerome Novosad <hello@popzenith.com>
2017-03-21 14:38:38 -0400

    1.058

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ WormBurner::Application.configure do
     :address        => ENV['POSTMARK_SMTP_SERVER'],
     :user_name      => ENV['POSTMARK_API_TOKEN'],
     :password       => ENV['POSTMARK_API_TOKEN'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'heroku.com',
     :authentication => :cram_md5, # or :plain for plain-text authentication
     :enable_starttls_auto => true, # or false for unencrypted connection
   }
